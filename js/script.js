de249528ba97750702c1d9f5593abca4a6906238
vi_ser <vinceulisse@gmail.com>
2024-02-08 12:25:23 +0100

    funzione che genera bombe

--- js/script.js
+++ js/script.js
@@ -1,20 +1,23 @@
 /*
-L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
-Ogni cella ha un numero progressivo, da 1 a 100.
-Ci saranno quindi 10 caselle per ognuna delle 10 righe.
-Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
-Potete realizzare lo stile che volete, sin quando tutto è leggibile con facilità :muscolo::carnagione-2:
-Bonus
-Aggiungere una select accanto al bottone di generazione, che fornisca una scelta tra tre diversi livelli di difficoltà:
-- con difficoltà 1 => 100 caselle, con un numero compreso tra 1 e 100, divise in 10 caselle per 10 righe;
-- con difficoltà 2 => 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
-- con difficoltà 3 => 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;
+Il computer deve generare 16 numeri casuali e inserirli in un array, in base al range della difficoltà prescelta (se abbiamo scelto facile l'array conterrà numeri casuali da 1 a 100, se invece abbiamo scelto difficile l'array dovrà contenerne da 1 a 49): questi rappreseranno le posizioni delle nostre bombe.
+Attenzione: nella stessa cella può essere posizionata al massimo una bomba, perciò nell’array delle bombe non potranno esserci due numeri uguali.
+In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina. Altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.
+La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile di numeri consentiti (ovvero quando ha rivelato tutte le celle che non sono bombe).
+Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
+
+BONUS 1
+Quando si clicca su una bomba e finisce la partita, evitare che si possa cliccare su altre celle.
+
+BONUS 2
+Quando si clicca su una bomba e finisce la partita, il software scopre tutte le bombe nascoste.
 */
 
 const gridElement = document.querySelector("#grid");
 const startButton = document.querySelector("#start");
 const difficultyEl = document.querySelector("#difficulty");
 
+const bombCells = [];
+const bombNumber = 16;
 
 // difficoltà di default su "easy"
 let selectedValue = difficultyEl.value;
@@ -57,7 +60,6 @@ startButton.addEventListener("click",
         }
 
 
-
         // generazione griglia
         for (let i = 0; i < gridSize; i++) {
 
@@ -95,16 +97,36 @@ startButton.addEventListener("click",
 
                     console.log(this);
                     // coloro la casella al click
-                    newCell.classList.toggle("active");
+                    newCell.classList.add("active");
 
                 }
             )
 
             // aggiungo cella alla griglia
             gridElement.append(newCell);
-
         }
+
+        // inserisco le celle bomba
+        bombMaker(gridSize);
+
     }
 )
+
+
+// generazione array con bombe in base alla grandezza della griglia
+function bombMaker(size) {
+
+    while (bombCells.length < bombNumber) {
+        const bomb = Math.floor(Math.random() * size + 1);
+
+        // controllo che non ci siano duplicati
+        if (!bombCells.includes(bomb)) {
+
+            // inserisco la bomba nell'array
+            bombCells.push(bomb);
+        }
+    }
+    console.log("Bombe: ", bombCells);
+}
 
 
